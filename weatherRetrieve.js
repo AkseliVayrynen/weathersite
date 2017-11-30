e29171f26bfbbc49b710c9f8be5e476f46f92ff0
Akseli Väyrynen <avayryne@csc.fi>
2017-11-30 15:37:19 +0200

    JS edit

--- weatherRetrieve.js
+++ weatherRetrieve.js
@@ -14,7 +14,6 @@ $(document).ready(function(){
     function getWeatherJSON() {
         retrieveNumber++;
         
-        $.getJSON("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=8e471c6c981142024e3c7cef55d99d2d", function(weatherData) {
             temperature = Math.round(weatherData.list[0].main.temp - 273.15);
             description = weatherData.list[0].weather[0].description;
         });
