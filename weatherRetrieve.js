83a5635a1c720b60dccf583987c28e4d4a2556c5
Akseli Väyrynen <avayryne@csc.fi>
2017-11-30 15:35:04 +0200

    Debugging

--- weatherRetrieve.js
+++ weatherRetrieve.js
@@ -10,15 +10,17 @@ $(document).ready(function(){
     
     var description;
     var temperature;
+    
     function getWeatherJSON() {
         retrieveNumber++;
-
+        
+        $.getJSON("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=8e471c6c981142024e3c7cef55d99d2d", function(weatherData) {
             temperature = Math.round(weatherData.list[0].main.temp - 273.15);
             description = weatherData.list[0].weather[0].description;
         });
-   
+    
+
         console.log("Current weather data retrieved, this was attempt number: " + retrieveNumber);
-        console.log(description);
         enterData();
             
     }
