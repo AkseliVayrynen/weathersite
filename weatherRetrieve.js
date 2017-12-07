e5aa371ad6b908c3ab93bbc60fff2aa8b12e6ca8
Akseli Väyrynen <avayryne@csc.fi>
2017-12-07 14:08:48 +0200

    Updating the icon now works

--- weatherRetrieve.js
+++ weatherRetrieve.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
     //How many times has JSON been retrieved?
     var retrieveNumber = 0;
     
-    var description;
     var temperature;
     var weatherId;
     
@@ -13,9 +12,8 @@ $(document).ready(function(){
     function getWeatherJSON() {
         retrieveNumber++;
         
-        $.getJSON(" http://api.openweathermap.org/data/2.5/forecast?id=660158&APPID=***************", function(weatherData) {
+        $.getJSON(" http://api.openweathermap.org/data/2.5/forecast?id=660158&APPID=***********************", function(weatherData) {
             temperature = Math.round(weatherData.list[0].main.temp - 273.15);
-            description = weatherData.list[0].weather[0].description;
             weatherId = weatherData.list[0].weather[0].id;
         });
     
@@ -29,8 +27,7 @@ $(document).ready(function(){
     
     //Handles the received id for weather and inserts an according icon. Only "usual" weather conditions are handled, thus we leave extremes out. 
     function handleWeatherIcon(id) {
-        
-        var weatherIcon = document.createElement("IMG");
+        var iconSource;
 
         if (id >= 200 && id <= 232) { //Thunderstorms
             console.log("Thunderstorms");
@@ -38,8 +35,7 @@ $(document).ready(function(){
             console.log("Drizzle");
         } else if (id >= 500 && id <= 531) { //Rain 
             console.log("Rain");
-            //document.getElementById("description").innerHTML = "It Rains!";
-            weatherIcon.setAttribute("src", "http://openweathermap.org/img/w/09d.png");
+            iconSource = "http://openweathermap.org/img/w/09d.png";
         } else if (id >= 600 && id <= 622) { //Snow
             console.log("Snow");
         } else if (id === 800) { //Sunny
@@ -50,7 +46,9 @@ $(document).ready(function(){
             console.log("The id received from OpenWeatherMap could not be used at this time. There might be some extreme conditions going on, for instance.");
         }
         
-        document.getElementById("description").appendChild(weatherIcon);
+        if (iconSource != undefined) {
+            document.getElementById("weatherIcon").src = iconSource;
+        }
     }
 
 
