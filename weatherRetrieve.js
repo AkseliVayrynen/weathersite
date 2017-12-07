72f719d076e08f51f68fcb962e470a7f53ab0c4c
Akseli Väyrynen <avayryne@csc.fi>
2017-12-07 14:54:00 +0200

    JS now handles the basic weather conditions

--- weatherRetrieve.js
+++ weatherRetrieve.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
     function getWeatherJSON() {
         retrieveNumber++;
         
-        $.getJSON(" http://api.openweathermap.org/data/2.5/forecast?id=660158&APPID=***********************", function(weatherData) {
+        $.getJSON(" http://api.openweathermap.org/data/2.5/forecast?id=660158&APPID=****************************", function(weatherData) {
             temperature = Math.round(weatherData.list[0].main.temp - 273.15);
             weatherId = weatherData.list[0].weather[0].id;
         });
@@ -31,19 +31,34 @@ $(document).ready(function(){
 
         if (id >= 200 && id <= 232) { //Thunderstorms
             console.log("Thunderstorms");
+            iconSource = "http://openweathermap.org/img/w/11d.png";
+            
         } else if (id >= 300 && id <= 321) { //Drizzle
             console.log("Drizzle");
+            iconSource = "http://openweathermap.org/img/w/09d.png";
+            
         } else if (id >= 500 && id <= 531) { //Rain 
             console.log("Rain");
             iconSource = "http://openweathermap.org/img/w/09d.png";
+            
         } else if (id >= 600 && id <= 622) { //Snow
             console.log("Snow");
+            iconSource = "http://openweathermap.org/img/w/13d.png";
+            
         } else if (id === 800) { //Sunny
             console.log("Sunny");
-        } else if (id >= 801 && id <= 804) { //Clouds
+            iconSource = "http://openweathermap.org/img/w/01d.png";
+            
+        } else if (id ==== 801) { //Few clouds
+            console.log("Few clouds");
+            iconSource = "http://openweathermap.org/img/w/02d.png";
+            
+        } else if (id >= 802 && id <= 804) { //Clouds
             console.log("Clouds");
+            iconSource = "http://openweathermap.org/img/w/03d.png";
+            
         } else {
-            console.log("The id received from OpenWeatherMap could not be used at this time. There might be some extreme conditions going on, for instance.");
+            console.log("The id received from OpenWeatherMap could not be used at this time. There might be some extreme conditions going on, for instance. This can also occur when the weather data is retrieved for the first time.");
         }
         
         if (iconSource != undefined) {
